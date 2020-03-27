9002bb51a9d71af64c0baf474a9f519add7cbe2d
Chaney <52712847+chanbro@users.noreply.github.com>
2020-03-27 17:52:37 -0500

    first commit

---
+++ src/main/java/com/capstone/kitsune/KitsuneApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.kitsune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitsuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitsuneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/kitsune/KitsuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.kitsune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitsuneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
