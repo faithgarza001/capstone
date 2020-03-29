bd9be64e08bb24e99f5b1931301890982efc5058
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-29 13:37:44 -0500

    created registration and create posts functionality for now

---
+++ src/main/java/com/capstone/kitsune/controllers/PostController.java
@@ -0,0 +1,31 @@
+package com.capstone.kitsune.controllers;
+
+import com.capstone.kitsune.models.Post;
+import com.capstone.kitsune.repositories.PostRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PostController {
+    private PostRepo postDao;
+
+    public PostController(PostRepo postDao){
+        this.postDao = postDao;
+    }
+
+    @GetMapping("/dashboard/create")
+    public String showCreateForm(Model model) {
+            model.addAttribute("post", new Post());
+            return "posts/create";
+    }
+
+    @PostMapping("/dashboard/create")
+    public String postNewPost(@RequestParam String title, @RequestParam String body) {
+        Post post = new Post(title, body, loggedInUser);
+        postDao.save(post);
+        return "redirect:/posts";
+    }
+}
---
+++ src/main/java/com/capstone/kitsune/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.capstone.kitsune.controllers;
+
+import com.capstone.kitsune.models.User;
+import com.capstone.kitsune.repositories.UserRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UserRepo users;
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        users.save(user);
+        return "redirect:/login";
+    }
+}
