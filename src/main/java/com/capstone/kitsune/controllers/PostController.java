5f908c33fa0844c89884d1e0c8e4e20150b39649
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-30 14:40:41 -0500

    adding loggedinuser variable

--- src/main/java/com/capstone/kitsune/controllers/PostController.java
+++ src/main/java/com/capstone/kitsune/controllers/PostController.java
@@ -1,7 +1,10 @@
 package com.capstone.kitsune.controllers;
 
 import com.capstone.kitsune.models.Post;
+import com.capstone.kitsune.models.User;
 import com.capstone.kitsune.repositories.PostRepo;
+import com.capstone.kitsune.repositories.UserRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,19 +14,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PostController {
     private PostRepo postDao;
+    private UserRepo userDao;
 
-    public PostController(PostRepo postDao){
+    public PostController(PostRepo postDao, UserRepo userDao){
         this.postDao = postDao;
+        this.userDao = userDao;
     }
 
-    @GetMapping("/dashboard/create")
+    @GetMapping("/dashboard/posts/create")
     public String showCreateForm(Model model) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(loggedInUser != null) {
             model.addAttribute("post", new Post());
             return "posts/create";
+        } else{
+            return "redirect:/login";
+        }
     }
 
-    @PostMapping("/dashboard/create")
+    @PostMapping("/dashboard/posts/create")
     public String postNewPost(@RequestParam String title, @RequestParam String body) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Post post = new Post(title, body);
         postDao.save(post);
         return "redirect:/dashboard";
