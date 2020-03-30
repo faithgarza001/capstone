60fca4e6b6e989ab0a2aaaab9782730a06acd9fa
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-30 16:06:04 -0500

    forrealzies URLs

--- src/main/java/com/capstone/kitsune/SecurityConfiguration.java
+++ src/main/java/com/capstone/kitsune/SecurityConfiguration.java
@@ -52,8 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(
-                        "/dashboard", "/dashboard/posts/create", "/dashboard/blogs/create", "/dashboard/blogs/{id}/edit", "/dashboard/posts/{id}/edit", "/dashboard/posts/{id}", "/dashboard/blogs/{id}", "/dashboard/posts/{id}/delete", "/dashboard/blogs/{id}/delete"// only authenticated users can edit ads
-                )
+                        "/dashboard", "/dashboard/posts/create", "/dashboard/blogs/create", "/dashboard/blogs/{id}/edit", "/dashboard/posts/{id}/edit", "/dashboard/posts/{id}", "/dashboard/blogs/{id}", "/dashboard/posts/{id}/delete", "/dashboard/blogs/{id}/delete", "/users/myaccount", "/users/myaccount/{id}/edit")
                 .authenticated()
         ;
     }
