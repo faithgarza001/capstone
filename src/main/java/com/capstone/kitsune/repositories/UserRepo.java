bdae158a4b4888f6dd80557652d60385e3ad54e5
faithgarza <faith.garza001@gmail.com>
2020-03-30 18:41:50 -0500

    added userdetailsloader in services

--- src/main/java/com/capstone/kitsune/repositories/UserRepo.java
+++ src/main/java/com/capstone/kitsune/repositories/UserRepo.java
@@ -4,4 +4,7 @@ import com.capstone.kitsune.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
 }
