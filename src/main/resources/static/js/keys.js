cbfa0b43fe6b309d8ce3da3f947ea9bf9fd9f6b0
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-04-08 07:25:06 -0500

    small changes

--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-var youtubeToken = "AIzaSyClDdgcLzDzJoyq4XU6nuZ7MK8BML9QpGI";
-=======
-googleAPIs = "AIzaSyC_2rCK9MjyZM0k837224Z_W7yTXluJiWk"
->>>>>>> chaney
+let googleAPIs = "AIzaSyC_2rCK9MjyZM0k837224Z_W7yTXluJiWk";
+
